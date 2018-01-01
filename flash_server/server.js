63c23420e5297b4c6509f4299a91ec516bcdf22a
Johannes Innerbichler <j.innerbichler@gmail.com>
2018-01-01 18:57:25 +0100

    added iota lib

--- flash_server/server.js
+++ flash_server/server.js
@@ -1,12 +1,21 @@
 "use strict";
 const express = require('express');
+
 const bodyParser = require('body-parser');
+const IOTACrypto = require("iota.crypto.js");
+const transfers = require("iota.flash.js").transfer;
+const multisig = require("iota.flash.js").multisig;
+const flashHelper = require("./helper");
 
 let app = express();
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended: false}));
 
+app.get('/', function(req, res) {
+    res.send('hell0');
+});
+
 app.listen(3000, function () {
-    console.log('Example app listening on port 3000!');
+    console.log('Flash server listening on port 3000!');
 });
